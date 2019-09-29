4dc30de79e8109b47950ef45aaf098b07ab91541
DanilTremasov <48184229+DanilTremasov@users.noreply.github.com>
2019-09-29 13:13:51 +0300

    Update hw1_winversion.py

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -7,17 +7,15 @@ def word_inversion(input_lst):
     n = 0
     for i in range(len(input_lst)):
         if input_lst[i] == ' ':
-            check = i
-            tmp = check
-            for k in range(n, n + ((check - n + 1) // 2)):
+            tmp = i
+            for k in range(n, n + ((i - n + 1) // 2)):
                 input_lst[n], input_lst[tmp-1] = input_lst[tmp-1], input_lst[n]
                 tmp = tmp - 1
                 n = n + 1
-            n = check + 1
+            n = i + 1
         if i == (len(input_lst)-1):
             tmp = i
-            check = tmp
-            for j in range(n, n + ((check - n + 1) // 2)):
+            for j in range(n, n + ((i - n + 1) // 2)):
                 input_lst[n], input_lst[tmp] = input_lst[tmp], input_lst[n]
                 tmp = tmp - 1
                 n = n + 1
